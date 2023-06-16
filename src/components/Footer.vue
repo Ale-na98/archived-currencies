8e38ebd2c9e7b1635cd269c12326afcd20da5c88
Alena Navrotskaia <Ale-na98@yandex.ru>
2023-06-16 17:59:10 +0200

    Update Footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,46 @@
 <template>
-    <footer class="footer mt-auto">
-        <div class="container">
-            <div class="row justify-content-between">
-                <div class="col-lg-4">
-                    <p>
-                        <strong>Contact Us:</strong>
-                        <br>
-                        E-mail: archived-currencies@gmail.com
-                        <br>
-                        Phone number: +(48) 123 123 123
-                    </p>
-                </div>
-            </div>
-            <div class="footer-divider"></div>
-            <div class="text-center">
+    <footer class="footer">
+        <div class="container py-3">
+            <div class="col col-sm-6 col-md-5 col-lg-3">
+                <span class="footer-header">ARCHIVED EXCHANGE RATES</span>
+                <div class="footer-divider"></div>
                 <p>
-                    <span>Copyright © 2023 </span>
-                    <span>Archived Currencies. </span>
+                    Designed and built with all the love in the world by
+                    <a href="https://github.com/Ale-na98/archived-exchange-rates">Ale-na98</a>.
                 </p>
             </div>
-    </div>
-</footer></template>
+        </div>
+    </footer>
+</template>
+
+<style>
+.footer {
+    color: #fff;
+    background-color: #152e52;
+}
+
+.footer-header {
+    color: #bdad7d;
+    font-size: 17px;
+    font-weight: 500;
+}
+
+.footer a {
+    color: #bdad7d;
+    text-decoration: none;
+
+}
+
+.footer a:hover {
+    color: #fff;
+    text-decoration: underline;
+}
+
+.footer-divider {
+    width: 100%;
+    opacity: .2;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    border-bottom: 1px solid #e8e8e8;
+}
+</style>
